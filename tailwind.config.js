2b93941e1a115ce10ba6e5a2077d136d8f4ed3e9
gefm2002 <fernandez.gaston.e@gmail.com>
2026-01-20 09:01:58 -0300

    Update landing changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         'mint-dark': '#6FA8A0',
         petrol: '#2F4F5A',
         'light-gray': '#F2F2F2',
+        brand: '#81BAB4',
       },
       boxShadow: {
         soft: '0 10px 30px rgba(47, 79, 90, 0.12)',
